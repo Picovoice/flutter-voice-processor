9d531abf5eeda7f24fdfef6298498ce80466746f
Ian Lavery <ian@picovoice.ai>
2020-12-07 13:54:36 -0800

    cleanup

--- android/src/main/java/ai/picovoice/flutter/voiceprocessor/FlutterVoiceProcessorHandler.java
+++ android/src/main/java/ai/picovoice/flutter/voiceprocessor/FlutterVoiceProcessorHandler.java
@@ -7,21 +7,18 @@ import android.content.pm.PackageManager;
 import android.media.AudioFormat;
 import android.media.AudioRecord;
 import android.media.MediaRecorder;
+import android.os.Handler;
 import android.os.Process;
 import android.util.Log;
-import android.os.Handler;
-
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
-
-import io.flutter.plugin.common.MethodCall;
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
-import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.EventChannel;
 import io.flutter.plugin.common.EventChannel.EventSink;
 import io.flutter.plugin.common.EventChannel.StreamHandler;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -31,43 +28,48 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-public class FlutterVoiceProcessorHandler implements MethodCallHandler, StreamHandler,
-PluginRegistry.RequestPermissionsResultListener {
-  private static final String LOG_TAG = "FlutterVoiceProcessorPlugin";      
-  private static final int RECORD_AUDIO_REQUEST_CODE = FlutterVoiceProcessorHandler.class.hashCode();
+public class FlutterVoiceProcessorHandler
+  implements
+    MethodCallHandler,
+    StreamHandler,
+    PluginRegistry.RequestPermissionsResultListener {
+
+  private static final String LOG_TAG = "FlutterVoiceProcessorPlugin";
+  private static final int RECORD_AUDIO_REQUEST_CODE =
+    FlutterVoiceProcessorHandler.class.hashCode();
 
   private final AtomicBoolean started = new AtomicBoolean(false);
   private final AtomicBoolean stop = new AtomicBoolean(false);
   private final AtomicBoolean stopped = new AtomicBoolean(false);
-  
+
   private final Activity activity;
-  private final Handler eventHandler = new Handler();    
+  private final Handler eventHandler = new Handler();
   private Result pendingPermissionResult;
   private EventSink bufferEventSink;
 
-  FlutterVoiceProcessorHandler(Activity activity) {    
+  FlutterVoiceProcessorHandler(Activity activity) {
     this.activity = activity;
   }
 
-  void close() {    
+  void close() {
     stop();
     pendingPermissionResult = null;
   }
 
   @Override
-  public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {    
+  public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
     switch (call.method) {
-      case "start" :
+      case "start":
         final Integer frameLength = call.argument("frameLength");
         final Integer sampleRate = call.argument("sampleRate");
         final boolean didStart = start(frameLength, sampleRate);
         result.success(didStart);
         break;
-      case "stop" :
+      case "stop":
         final boolean didStop = stop();
         result.success(didStop);
         break;
-      case "hasRecordAudioPermission" :
+      case "hasRecordAudioPermission":
         checkRecordAudioPermission(result);
         break;
       default:
@@ -84,28 +86,32 @@ PluginRegistry.RequestPermissionsResultListener {
   public void onCancel(Object listener) {
     bufferEventSink = null;
   }
-  
-  
+
   public boolean start(final Integer frameSize, final Integer sampleRate) {
-    
     if (started.get()) {
       return true;
     }
-    
-    Executors.newSingleThreadExecutor().submit(new Callable<Void>() {
-      @Override
-      public Void call() {
-        android.os.Process.setThreadPriority(Process.THREAD_PRIORITY_URGENT_AUDIO);
-        read(frameSize, sampleRate);
-        return null;
-      }
-    });
 
-    while(!started.get()){
+    Executors
+      .newSingleThreadExecutor()
+      .submit(
+        new Callable<Void>() {
+          @Override
+          public Void call() {
+            android.os.Process.setThreadPriority(
+              Process.THREAD_PRIORITY_URGENT_AUDIO
+            );
+            read(frameSize, sampleRate);
+            return null;
+          }
+        }
+      );
+
+    while (!started.get()) {
       try {
         Thread.sleep(10);
       } catch (InterruptedException e) {
-        Log.e(LOG_TAG, e.toString());        
+        Log.e(LOG_TAG, e.toString());
       }
     }
 
@@ -123,31 +129,47 @@ PluginRegistry.RequestPermissionsResultListener {
       try {
         Thread.sleep(10);
       } catch (InterruptedException e) {
-        Log.e(LOG_TAG, e.toString());        
+        Log.e(LOG_TAG, e.toString());
       }
     }
 
     started.set(false);
     stop.set(false);
-    stopped.set(false);    
+    stopped.set(false);
     return true;
   }
-  
+
   private void checkRecordAudioPermission(@NonNull Result result) {
-    boolean isPermissionGranted = ActivityCompat.checkSelfPermission(activity, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED;
+    boolean isPermissionGranted =
+      ActivityCompat.checkSelfPermission(
+        activity,
+        Manifest.permission.RECORD_AUDIO
+      ) ==
+      PackageManager.PERMISSION_GRANTED;
     if (!isPermissionGranted) {
       pendingPermissionResult = result;
-      ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.RECORD_AUDIO}, RECORD_AUDIO_REQUEST_CODE);
+      ActivityCompat.requestPermissions(
+        activity,
+        new String[] { Manifest.permission.RECORD_AUDIO },
+        RECORD_AUDIO_REQUEST_CODE
+      );
     } else {
       result.success(true);
     }
   }
 
   @Override
-  public boolean onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+  public boolean onRequestPermissionsResult(
+    int requestCode,
+    String[] permissions,
+    int[] grantResults
+  ) {
     if (requestCode == RECORD_AUDIO_REQUEST_CODE) {
       if (pendingPermissionResult != null) {
-        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+        if (
+          grantResults.length > 0 &&
+          grantResults[0] == PackageManager.PERMISSION_GRANTED
+        ) {
           pendingPermissionResult.success(true);
         } else {
           pendingPermissionResult.error("-2", "Permission denied", null);
@@ -161,50 +183,53 @@ PluginRegistry.RequestPermissionsResultListener {
   }
 
   private void read(final Integer frameSize, final Integer sampleRate) {
-    
     final int minBufferSize = AudioRecord.getMinBufferSize(
       sampleRate,
       AudioFormat.CHANNEL_IN_MONO,
-      AudioFormat.ENCODING_PCM_16BIT);
+      AudioFormat.ENCODING_PCM_16BIT
+    );
     final int bufferSize = Math.max(sampleRate / 2, minBufferSize);
-    final short[] buffer = new short[frameSize];    
+    final short[] buffer = new short[frameSize];
     AudioRecord audioRecord = null;
     try {
-      audioRecord = new AudioRecord(
-        MediaRecorder.AudioSource.MIC,
-        sampleRate,
-        AudioFormat.CHANNEL_IN_MONO,
-        AudioFormat.ENCODING_PCM_16BIT,
-        bufferSize);
+      audioRecord =
+        new AudioRecord(
+          MediaRecorder.AudioSource.MIC,
+          sampleRate,
+          AudioFormat.CHANNEL_IN_MONO,
+          AudioFormat.ENCODING_PCM_16BIT,
+          bufferSize
+        );
 
       audioRecord.startRecording();
-      boolean firstBuffer = true;      
+      boolean firstBuffer = true;
       while (!stop.get()) {
-        if (audioRecord.read(buffer, 0, buffer.length) == buffer.length) {          
-          if(firstBuffer){            
+        if (audioRecord.read(buffer, 0, buffer.length) == buffer.length) {
+          if (firstBuffer) {
             started.set(true);
             firstBuffer = false;
           }
-          
-          final ArrayList<Short> bufferObj = new ArrayList();
-          for (int i = 0; i < buffer.length; i++)
-            bufferObj.add(buffer[i]);
 
-          // send buffer event                      
-          eventHandler.post(new Runnable() {
-            @Override
-            public void run() {
-              if(bufferEventSink != null){
-                bufferEventSink.success(bufferObj);
+          final ArrayList<Short> bufferObj = new ArrayList();
+          for (int i = 0; i < buffer.length; i++) bufferObj.add(buffer[i]);
+
+          // send buffer event
+          eventHandler.post(
+            new Runnable() {
+              @Override
+              public void run() {
+                if (bufferEventSink != null) {
+                  bufferEventSink.success(bufferObj);
+                }
               }
             }
-          });                  
+          );
         }
       }
 
       audioRecord.stop();
-    } catch (IllegalArgumentException | IllegalStateException e) {      
-      Log.e(LOG_TAG, e.toString());      
+    } catch (IllegalArgumentException | IllegalStateException e) {
+      Log.e(LOG_TAG, e.toString());
     } finally {
       if (audioRecord != null) {
         audioRecord.release();
--- android/src/main/java/ai/picovoice/flutter/voiceprocessor/FlutterVoiceProcessorPlugin.java
+++ android/src/main/java/ai/picovoice/flutter/voiceprocessor/FlutterVoiceProcessorPlugin.java
@@ -1,19 +1,19 @@
 package ai.picovoice.flutter.voiceprocessor;
 
-import androidx.annotation.NonNull;
 import android.util.Log;
-
+import androidx.annotation.NonNull;
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.embedding.engine.plugins.activity.ActivityAware;
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
 import io.flutter.plugin.common.BinaryMessenger;
-import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.EventChannel;
+import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
-public class FlutterVoiceProcessorPlugin implements FlutterPlugin, ActivityAware {
+public class FlutterVoiceProcessorPlugin
+  implements FlutterPlugin, ActivityAware {
 
-  private static final String LOG_TAG = "FlutterVoiceProcessorPlugin";    
+  private static final String LOG_TAG = "FlutterVoiceProcessorPlugin";
 
   private MethodChannel methodChannel;
   private EventChannel eventChannel;
@@ -24,41 +24,54 @@ public class FlutterVoiceProcessorPlugin implements FlutterPlugin, ActivityAware
   @Override
   public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
     pluginBinding = binding;
-  }   
+  }
 
   @Override
   public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
     pluginBinding = null;
-  }    
+  }
 
   @Override
   public void onAttachedToActivity(ActivityPluginBinding binding) {
-    activityBinding = binding;    
-    voiceProcessorHandler = new FlutterVoiceProcessorHandler(binding.getActivity());    
+    activityBinding = binding;
+    voiceProcessorHandler =
+      new FlutterVoiceProcessorHandler(binding.getActivity());
     activityBinding.addRequestPermissionsResultListener(voiceProcessorHandler);
 
-    methodChannel = new MethodChannel(pluginBinding.getBinaryMessenger(), "flutter_voice_processor_methods");
+    methodChannel =
+      new MethodChannel(
+        pluginBinding.getBinaryMessenger(),
+        "flutter_voice_processor_methods"
+      );
     methodChannel.setMethodCallHandler(voiceProcessorHandler);
-    
-    eventChannel = new EventChannel(pluginBinding.getBinaryMessenger(), "flutter_voice_processor_events");
-    eventChannel.setStreamHandler(voiceProcessorHandler);    
+
+    eventChannel =
+      new EventChannel(
+        pluginBinding.getBinaryMessenger(),
+        "flutter_voice_processor_events"
+      );
+    eventChannel.setStreamHandler(voiceProcessorHandler);
   }
 
   @Override
   public void onDetachedFromActivity() {
-    activityBinding.removeRequestPermissionsResultListener(voiceProcessorHandler);
+    activityBinding.removeRequestPermissionsResultListener(
+      voiceProcessorHandler
+    );
     activityBinding = null;
-    methodChannel.setMethodCallHandler(null);    
+    methodChannel.setMethodCallHandler(null);
     eventChannel.setStreamHandler(null);
     voiceProcessorHandler = null;
     methodChannel = null;
   }
 
   @Override
-  public void onReattachedToActivityForConfigChanges(ActivityPluginBinding binding) {
+  public void onReattachedToActivityForConfigChanges(
+    ActivityPluginBinding binding
+  ) {
     onAttachedToActivity(binding);
   }
-  
+
   @Override
   public void onDetachedFromActivityForConfigChanges() {
     onDetachedFromActivity();
