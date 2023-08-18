47e890837cf0fc243e519ad7aebea8ae269aa3ba
Ian <ian@picovoice.ai>
2023-08-18 16:48:25 -0700

    fix android

--- example/integration_test/driver.dart
+++ example/integration_test/driver.dart
@@ -5,24 +5,21 @@ import 'package:integration_test/integration_test_driver.dart';
 
 Future<void> main() async {
   integrationDriver();
+  final Map<String, String> envVars = Platform.environment;
+  String? adbPath = join(
+    envVars['ANDROID_SDK_ROOT'] ?? envVars['ANDROID_HOME']!,
+    'platform-tools',
+    Platform.isWindows ? 'adb.exe' : 'adb',
+  );
 
-  if (Platform.isAndroid) {
-    final Map<String, String> envVars = Platform.environment;
-    String? adbPath = join(
-      envVars['ANDROID_SDK_ROOT'] ?? envVars['ANDROID_HOME']!,
-      'platform-tools',
-      Platform.isWindows ? 'adb.exe' : 'adb',
-    );
-
-    await Process.run(
-      adbPath,
-      [
-        'shell',
-        'pm',
-        'grant',
-        'ai.picovoice.flutter.voiceprocessorexample',
-        'android.permission.RECORD_AUDIO'
-      ],
-    );
-  }
+  await Process.run(
+    adbPath,
+    [
+      'shell',
+      'pm',
+      'grant',
+      'ai.picovoice.flutter.voiceprocessorexample',
+      'android.permission.RECORD_AUDIO'
+    ],
+  );
 }
