82fa6cb49ceb930f2ef1393950928f7a858b746d
Ian <ian@picovoice.ai>
2023-08-18 13:05:35 -0700

    fix codestyle

--- example/integration_test/app_test.dart
+++ example/integration_test/app_test.dart
@@ -1,12 +1,12 @@
-import 'dart:convert';
-import 'dart:io';
-import 'dart:typed_data';
+// import 'dart:convert';
+// import 'dart:io';
+// import 'dart:typed_data';
 
-import 'package:flutter/services.dart' show rootBundle;
+// import 'package:flutter/services.dart' show rootBundle;
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
-import 'package:flutter_voice_processor/flutter_voice_processor.dart';
+// import 'package:flutter_voice_processor/flutter_voice_processor.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
