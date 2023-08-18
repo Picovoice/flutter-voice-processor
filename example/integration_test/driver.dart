74561275705d1e8575f7410d91a7fc4f0bce82b1
Ian <ian@picovoice.ai>
2023-08-18 15:48:23 -0700

    update driver

--- example/integration_test/driver.dart
+++ example/integration_test/driver.dart
@@ -1,10 +1,19 @@
 import 'dart:io';
+
+import 'package:path/path.dart';
 import 'package:integration_test/integration_test_driver.dart';
 
 Future<void> main() async {
   integrationDriver();
+  final Map<String, String> envVars = Platform.environment;
+  String? adbPath = join(
+    envVars['ANDROID_SDK_ROOT'] ?? envVars['ANDROID_HOME']!,
+    'platform-tools',
+    Platform.isWindows ? 'adb.exe' : 'adb',
+  );
+
   await Process.run(
-    'adb',
+    adbPath,
     [
       'shell',
       'pm',
