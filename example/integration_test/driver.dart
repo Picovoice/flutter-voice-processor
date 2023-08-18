953f77f623c044bc44bf502f3aa40d0bd2975a9e
Ian <ian@picovoice.ai>
2023-08-18 16:14:01 -0700

    add permission to ios

--- example/integration_test/driver.dart
+++ example/integration_test/driver.dart
@@ -5,21 +5,35 @@ import 'package:integration_test/integration_test_driver.dart';
 
 Future<void> main() async {
   integrationDriver();
-  final Map<String, String> envVars = Platform.environment;
-  String? adbPath = join(
-    envVars['ANDROID_SDK_ROOT'] ?? envVars['ANDROID_HOME']!,
-    'platform-tools',
-    Platform.isWindows ? 'adb.exe' : 'adb',
-  );
 
-  await Process.run(
-    adbPath,
-    [
-      'shell',
-      'pm',
-      'grant',
-      'ai.picovoice.flutter.voiceprocessorexample',
-      'android.permission.RECORD_AUDIO'
-    ],
-  );
+  if (Platform.isAndroid) {
+    final Map<String, String> envVars = Platform.environment;
+    String? adbPath = join(
+      envVars['ANDROID_SDK_ROOT'] ?? envVars['ANDROID_HOME']!,
+      'platform-tools',
+      Platform.isWindows ? 'adb.exe' : 'adb',
+    );
+
+    await Process.run(
+      adbPath,
+      [
+        'shell',
+        'pm',
+        'grant',
+        'ai.picovoice.flutter.voiceprocessorexample',
+        'android.permission.RECORD_AUDIO'
+      ],
+    );
+  } else if (Platform.isIOS) {
+    await Process.run(
+      'applesimutils',
+      [
+        '--booted',
+        '--bundle',
+        'ai.picovoice.flutter.voiceprocessor.flutterVoiceProcessorExample',
+        '--setPermissions',
+        'microphone=YES',
+      ],
+    );
+  }
 }
